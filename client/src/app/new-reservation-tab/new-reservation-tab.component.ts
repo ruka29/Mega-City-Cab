7656fe4a731a4cbdea5dbe8773e44f23fb43a333
Rukshan Kumara <rukshankumara2906@gmail.com>
2025-02-19 12:30:37 +0530

    new reservation UI almost done.

--- client/src/app/new-reservation-tab/new-reservation-tab.component.ts
+++ client/src/app/new-reservation-tab/new-reservation-tab.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-reservation-tab',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './new-reservation-tab.component.html',
   styleUrl: './new-reservation-tab.component.scss'
 })
 export class NewReservationTabComponent {
+  schedule: boolean = false;
+  currentDate: string = new Date().toISOString().split('T')[0];
+  currentTime: string = new Date().toTimeString().split(' ')[0].slice(0, 5);
 
+  setSchedule(event: Event) {
+    event.preventDefault();
+    this.schedule = !this.schedule;
+  }
 }
